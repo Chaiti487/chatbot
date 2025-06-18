74fe1a4eea3935e9d8012ae8bbb5cae8ce119f9b
Chaiti487 <chaitisen487@gmail.com>
2025-06-18 12:15:23 +0530

    change

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
+    data = request.get_json()
+    print(data)
     return "Hello"
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
